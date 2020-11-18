613cb5ce0e28b8834643f56f57ddf581b323aedf
Chandran996 <roxstar1ha@gmail.com>
2020-11-18 15:48:36 +0800

    Revert "Revert "gogo""
    This reverts commit f29373f8987adbbc9f11d4196a5498f76b1914ca.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,47 @@
 //USER DEFINED JS
+const POP_UP = "popup_id";
+
+$( document ).ready(function() 
+{
+    imagepopUp();
+});
+
+function imagepopUp()
+{
+    var thumbnails = document.getElementsByClassName("rounded figure-img img-fluid");
+    if (thumbnails !== null)
+    {
+        for (var i = 0; i < thumbnails.length; i++)
+        {
+            var thumbnail = thumbnails[i];
+            thumbnail.addEventListener("click", popUp);
+        }
+    }
+    else
+    {
+        console.log("No thumbnail images found!");
+    }
+}
+
+function popUp(e)
+{
+    var popup = document.getElementById(POP_UP);
+    
+    if (popup === null)
+    {
+        popup = document.createElement("span");
+        popup.id = POP_UP;
+        popup.setAttribute("class", "image_popup");
+        
+        var thumbnail = e.target;
+        var small_image = thumbnail.src;
+        var big_image = small_image.replace("_small", "_large");
+        
+        popup.innerHTML = '<img src=' + big_image + '>';
+        thumbnail.insertAdjacentElement("afterend", popup);
+    }
+    else
+    {
+        $("#" + POP_UP).remove();
+    }
+}
