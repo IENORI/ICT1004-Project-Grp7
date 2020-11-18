0e8f375137caea9cae210b661b26f4dac8b2e3d5
Chandran996 <roxstar1ha@gmail.com>
2020-11-18 16:01:08 +0800

    popup
    align image with popup

--- adopt.php
+++ adopt.php
@@ -28,23 +28,20 @@
         
         <main class="container">
             <div class="row">
-                <div class="col-sm-8" align="center">
+        <div class="col-sm"  align="center">
+            <figure class="figure">
+                <img src="media/image/image002.png" class="rounded figure-img img-fluid">
+                <figcaption class="figure-caption">$500</figcaption>
+            </figure>
+        </div>
+        <div class="col-sm" align="center">
+            <figure class="figure">
+                <img src="media/image/image003.png" class="rounded figure-img img-fluid">
+                <figcaption class="figure-caption">$500</figcaption>
+            </figure>
+        </div>                
                     
-                        <figure class="figure">
-                            <img src="media/image/image002.png" class="rounded figure-img img-fluid">
-                            <figcaption class="figure-caption">$500</figcaption>
-                        </figure>
-                </div>
-                 <div class="col-sm-8" align="center">
-                        <figure class="figure">
-                            <img src="media/image/image002.png" class="rounded figure-img img-fluid">
-                            <figcaption class="figure-caption">$500</figcaption>
-                        </figure>
-                 </div>
-                </div>
-                <div class="col-sm-4">
-                    
-                </div>
+               
 
         </main>
 
