82316c5a4f8eb18ce566173d039d26fb8cc8e3f0
junhui-f <fjh828@gmail.com>
2020-11-22 13:01:16 +0800

    Clean up old code

--- inc.nav.php
+++ inc.nav.php
@@ -29,31 +29,4 @@
         </ul>
     </div>
 </nav>
-<!--<header class="fixed-top page-header">
-    <div class="container-fluid container-fluid-max">
-        <nav id ="navbar" class="navbar navbar-expand-lg navbar-dark">
-            <a class="navbar-brand" href="/">
-                <img id="nav-logo" src="media/icon/cat.svg" width="30" height="30" alt="CAT">
-                Fancy Felines
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navBarToggle">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navBarToggle">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Store</a>
-                    </li>
-                </ul>
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="cart.php">
-                            <img id="nav-logo" src="media/icon/shopping_cart-white-48dp.svg" width="30" height="30" alt="CAT">
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </div>
-</header>-->
 
