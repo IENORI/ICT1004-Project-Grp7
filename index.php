82a43539e3fc4678807eaeedf893e198151c6748
junhui-f <fjh828@gmail.com>
2020-11-26 19:29:02 +0800

    Rehost "services" images locally

--- index.php
+++ index.php
@@ -15,10 +15,11 @@ include "inc.sessionauthless.php";
             <div class="hero-text">
                 <h3 style="color: white">ADOPTIONS NOW OPEN BY APPOINTMENT</h3>
                 <p>Adopt a cutie who will brighten your day!</p>
-                <form action="adopt.php">
-                    <button type="submit">More information > </button>
-                </form>
-                
+<!--                <form action="adopt.php">-->
+<!--                    <button class="btn btn-primary" type="submit">More information > </button>-->
+<!--                </form>-->
+                <a href="adopt.php" class="btn btn-primary px-5">Find out more!</a>
+
             </div>
         </div>
 
--- services.php
+++ services.php
@@ -2,26 +2,14 @@
     TODO:
         ADD THE IMAGES INTO OUR MEDIA DIRECTORY: http://www.pngall.com/kitten-png
 -->
-<?php
-include "inc.sessionauthless.php";
-?>
 
-
-<!--            <div id="breadcrumb-wrapper" class="container mt-3">
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="/">Home</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Services</li>
-                    </ol>
-                </nav>
-            </div>-->
 <div class="bg-light">
     <div class="container py-5">
         <div class="row h-100 align-items-center py-5">
             <div class="col-lg-6">
                 <h1 class="display-4">CAT SANCTUARY</h1>
                 <p class="lead">
-                    Can’t find a suitable companion? Please revisit our site as we update frequently. 
+                    Can’t find a suitable companion? Please revisit our site as we update frequently.
                     Alternatively, you may subscribe to our newsletter for updates.
                     If you are looking for a missing pet, please visit our lost & found page.
                 </p>
@@ -43,24 +31,24 @@ include "inc.sessionauthless.php";
                     Adoption information, Adoption Practices, Types of breed,
                     HDB-Approved, Licensing, Vaccination etc
                 </p>
-                <a href="#" class="btn btn-light px-5 rounded-pill shadow-sm">Learn More</a>
+                <a href="#" class="btn btn-primary">Learn More</a>
             </div>
             <div class="col-lg-5 px-5 mx-auto order-1 order-lg-2">
-                <img src="http://www.pngall.com/wp-content/uploads/2016/05/Kitten-PNG-File.png" alt="" class="img-fluid mb-4 mb-lg-0">
+                <img src="media/image/Kitten-PNG-File.png" alt="" class="img-fluid mb-4 mb-lg-0">
             </div>
         </div>
         <br>
         <!--BANNER 3-->
         <div class="row align-items-center">
             <div class="col-lg-5 px-5 mx-auto">
-                <img src="http://www.pngall.com/wp-content/uploads/2016/05/Kitten-High-Quality-PNG.png" alt="" class="img-fluid mb-4 mb-lg-0">
+                <img src="media/image/Kitten-High-Quality-PNG.png" alt="" class="img-fluid mb-4 mb-lg-0">
             </div>
             <div class="col-lg-6">
                 <h2 class="font-weight-light">Help us</h2>
                 <p class="font-italic text-muted mb-4">
                     Donation, Volunteer, Corporate, Individual etc
                 </p>
-                <a href="#" class="btn btn-light px-5 rounded-pill shadow-sm">Learn More</a>
+                <a href="#" class="btn btn-primary">Learn More</a>
             </div>
         </div>
     </div>
