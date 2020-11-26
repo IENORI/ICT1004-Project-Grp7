e565016b8e59818d4a9d91a07527c878716e1f2b
junhui-f <fjh828@gmail.com>
2020-11-26 21:45:46 +0800

    Cleaned up index "services" to fit responsive

--- services.php
+++ services.php
@@ -1,55 +1,40 @@
-<!--
-    TODO:
-        ADD THE IMAGES INTO OUR MEDIA DIRECTORY: http://www.pngall.com/kitten-png
--->
-
-<div class="bg-light">
-    <div class="container py-5">
-        <div class="row h-100 align-items-center py-5">
-            <div class="col-lg-6">
-                <h1 class="display-4">CAT SANCTUARY</h1>
-                <p class="lead">
-                    Can’t find a suitable companion? Please revisit our site as we update frequently.
-                    Alternatively, you may subscribe to our newsletter for updates.
-                    If you are looking for a missing pet, please visit our lost & found page.
-                </p>
-            </div>
-            <div class="col-lg-6 d-none d-lg-block">
-                <img src="media/image/Kitten-Free-PNG-Image.png" alt="" class="img-fluid">
-            </div>
-        </div>
+<div id="index_callout_1" class="row container-fluid p-4 m-0">
+    <div class="col-md-6 col-xl-4 container align-self-center text-center">
+        <h1>Cat Sanctuary</h1>
+        <p>
+            Can’t find a suitable companion? Please revisit our site as we update frequently.
+            Alternatively, you may subscribe to our newsletter for updates.
+            If you are looking for a missing pet, please visit our lost & found page.
+        </p>
+    </div>
+    <div class="col-md-6 col-xl-4 container align-self-center">
+        <img src="media/image/Kitten-PNG-File.png" alt="" class="img-fluid p-4 mx-auto d-block" style="max-height: 400px">
     </div>
 </div>
 
-<div class="bg-white py-5">
-    <div class="container py-5">
-        <!--BANNER 2-->
-        <div class="row align-items-center mb-5">
-            <div class="col-lg-6 order-2 order-lg-1">
-                <h2 class="font-weight-light">Frequently Asked Questions</h2>
-                <p class="font-italic text-muted mb-4">
-                    Adoption information, Adoption Practices, Types of breed,
-                    HDB-Approved, Licensing, Vaccination etc
-                </p>
-                <a href="#" class="btn btn-primary">Learn More</a>
-            </div>
-            <div class="col-lg-5 px-5 mx-auto order-1 order-lg-2">
-                <img src="media/image/Kitten-PNG-File.png" alt="" class="img-fluid mb-4 mb-lg-0">
-            </div>
-        </div>
-        <br>
-        <!--BANNER 3-->
-        <div class="row align-items-center">
-            <div class="col-lg-5 px-5 mx-auto">
-                <img src="media/image/Kitten-High-Quality-PNG.png" alt="" class="img-fluid mb-4 mb-lg-0">
-            </div>
-            <div class="col-lg-6">
-                <h2 class="font-weight-light">Help us</h2>
-                <p class="font-italic text-muted mb-4">
-                    Donation, Volunteer, Corporate, Individual etc
-                </p>
-                <a href="#" class="btn btn-primary">Learn More</a>
-            </div>
-        </div>
+<div id="index_callout_2" class="row container-fluid p-4 m-0 bg-light">
+    <div class="col-md-6 col-xl-4 container align-self-center">
+        <img src="media/image/Kitten-Free-PNG-Image.png" alt="" class="img-fluid p-4 mx-auto d-block" style="max-height: 400px">
+    </div>
+    <div class="col-md-6 col-xl-4 container align-self-center text-center">
+        <h1>Frequently Asked Questions</h1>
+        <p>
+            Adoption information, Adoption Practices, Types of breed,
+            HDB-Approved, Licensing, Vaccination
+        </p>
+        <a href="#" class="btn btn-primary">Learn More</a>
+    </div>
+</div>
+
+<div id="index_callout_3" class="row container-fluid p-4 m-0">
+    <div class="col-md-6 col-xl-4 container align-self-center text-center">
+        <h1>Help Us</h1>
+        <p>
+            Donation, Volunteer, Corporate, Individual etc
+        </p>
+        <a href="#" class="btn btn-primary">Learn More</a>
+    </div>
+    <div class="col-md-6 col-xl-4 container align-self-center">
+        <img src="media/image/Kitten-High-Quality-PNG.png" alt="" class="img-fluid p-4 mx-auto d-block" style="max-height: 400px">
     </div>
 </div>
