e4ff33facf6df2818325ace7eb0079430cde247e
junhui-f <fjh828@gmail.com>
2020-11-26 20:22:33 +0800

    Cleaned up hero banner

--- index.php
+++ index.php
@@ -5,28 +5,35 @@ include "inc.sessionauthless.php";
 <html lang="en">
     <head>
         <?php
-        include "inc.head.php";
-        include "inc.nav.php";
+            include "inc.head.php";
         ?>
+        <!--CSS-->
         <link rel="stylesheet" href="css/index_css.css">
     </head>
     <body>
-        <div class="hero-image">
-            <div class="hero-text">
-                <h3 style="color: white">ADOPTIONS NOW OPEN BY APPOINTMENT</h3>
-                <p>Adopt a cutie who will brighten your day!</p>
-<!--                <form action="adopt.php">-->
-<!--                    <button class="btn btn-primary" type="submit">More information > </button>-->
-<!--                </form>-->
-                <a href="adopt.php" class="btn btn-primary px-5">Find out more!</a>
+        <div id="parent-wrapper" class="d-flex flex-column min-vh-100">
+            <?php
+                include "inc.nav.php";
+            ?>
 
-            </div>
+            <!-- Main wrapper to help split from nav-->
+            <main id="content-wrapper" class="container-fluid p-0 m-0">
+                <!-- Banner -->
+                <div id="index_banner_image" class="row container-fluid p-0 m-0">
+                    <div id="index_banner_text" class="justify-content-center align-self-center container-fluid">
+                        <h3 class="text-white p-0">ADOPTIONS NOW OPEN BY APPOINTMENT</h3>
+                        <p class="text-white m-4">Adopt a cutie who will brighten your day!</p>
+                        <a href="adopt.php" class="btn btn-primary px-4">Find out more!</a>
+                    </div>
+                </div>
+                <?php
+                    include "services.php";
+                ?>
+            </main>
+
+            <?php
+                include "inc.footer.php";
+            ?>
         </div>
-
-        <?php
-        include "services.php";
-        include "inc.footer.php";
-        ?>
-
     </body>
 </html>
