380f56e24dd9634a6d4598686d10fca6ea050745
dandan2588 <danieltan.tan199889@gmail.com>
2020-11-28 22:42:28 +0800

    adopt.php
    added FAQ

--- adopt.php
+++ adopt.php
@@ -57,21 +57,34 @@ if (mysqli_connect_errno()) {
                 process!</h6>
             <h6 class="appointmentCat">Can't find a suitable companion? Please revisit our site as we update frequently!<h6>
                     </div>
-        <div class="container"> 
-            <br>
-            <button class="collapsible bg-primary">Adoption Information</button>
-            <div class="content">
-                <br>
-                <h4>How to Adopt</h4>
-                <hr>
-                <h6>Step 1: Browse adoption gallery</h6>
-                <br>
-                <h6>Step 2: Click on "Make appointment"</h6>
-                <br>
-                <h6>Step 3: Show up for your appointment on time</h6>
-                <br>
-            </div>
-        </div>
+                    <div class="container"> 
+                        <br>
+                        <button class="collapsible bg-primary">Adoption Information</button>
+                        <div class="content">
+                            <br>
+                            <h4>How to Adopt</h4>
+                            <hr>
+                            <h6>Step 1: Browse adoption gallery</h6>
+                            <br>
+                            <h6>Step 2: Click on "Make appointment"</h6>
+                            <br>
+                            <h6>Step 3: Show up for your appointment on time</h6>
+                            <br>
+                        </div>
+                        <button class="collapsible bg-primary">Frequently Asked Question (FAQs)</button>
+                        <div class="content">
+                            <br>
+                            <h6><b>- Is cats easy to take care of ?</b></h6>
+                            <h6>You betcha!</h6>
+                            <br>
+                            <h6><b>- Is cats the best thing ever ?</b></h6>
+                            <h6>They are definitely a bundle of joy!</h6>
+                            <br>
+                            <h6><b>- Will cat get sick easily ?</b></h6>
+                            <h6>With proper care they will definitely not get sick easily!</h6>
+                            <br>
+                        </div>
+                    </div>
                     <br>
 
                     <main class="container">
