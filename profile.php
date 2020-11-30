423f812bf6fcc0ac710cc8ded19e357685a2063f
junhui-f <fjh828@gmail.com>
2020-11-30 23:33:22 +0800

    Redesigned "Edit Profile"

--- profile.php
+++ profile.php
@@ -5,13 +5,13 @@
 <html lang="en">
     <head>
         <?php
-        include "inc.head.php";
+            include "inc.head.php";
         ?>
     </head>
     <body>
         <div id="parent-wrapper" class="d-flex flex-column min-vh-100">
             <?php
-            include "inc.nav.php";
+                include "inc.nav.php";
             ?>
 
             <div id="breadcrumb-wrapper" class="container mt-3">
@@ -64,12 +64,11 @@
                                         </div>
                                     </fieldset>
                                 </div>
-                            </div>
-                            <div class="card-footer container-fluid">
+                                <hr class="my-4">
                                 <div class="container-fluid">
                                     <a href="profileedit.php" class="btn btn-block btn-primary">Edit Account information</a>
                                 </div>
-                                <div class="container-fluid pt-2 text-center">
+                                <div class="container-fluid p-3 text-center">
                                     <a href="logout.php" class="btn btn-danger">Log out</a>
                                 </div>
                             </div>
--- profileedit.php
+++ profileedit.php
@@ -1,66 +1,69 @@
 <?php
     include "inc.sessionauth.php";
 ?>
-
-<html>
-    <header>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
         <?php
             include "inc.head.php";
         ?>
-    </header>
-    <div class="body">
-        <?php 
-            include "inc.nav.php";
-        ?>
-        <br>
-        <div class="container">
-            <br>
-            <div class="card">
-                <h4 class="card-header bg-success text-white">Edit Account Information</h4>
-                <div class="card-body" style="padding-bottom: 0">
-                    <form action="process/proc.profileedit.php" method="post">
-                        
-                        <div class="form-group">
-                            <label for="Edited_Email">Email Address: </label>
-                            <input required type="email" class="form-control" id="Edited_Email" name="Edited_Email"
-                                   value="<?php echo $Email ;?>">
-                            </input>
-                        </div>
-                        
-                        <hr class="my-4">
-                        
-                        <div class="form-group">
-                            <label for="Edit_Email">First Name: </label>
-                            <input required maxlength="45" type="text" class="form-control" id="Edited_Fname" name="Edited_Fname"
-                                   value="<?php echo $Fname ;?>">
-                        </div>
-                        
-                        <hr class="my-4">
-                        
-                        <div class="form-group">
-                            <label for="Edit_Email">Last Name: </label>
-                            <input required maxlength="45" type="text" class="form-control" id="Edited_Lname" name="Edited_Lname"
-                                   value="<?php echo $Lname ;?>">
-                        </div>
-                        
-                        <hr class="my-4">
-                        
-                        <div class="form-group">
-                            <label for="Edit_Email">Phone Number: </label>
-                            <input required max="99999999" min="60000000" type="number" class="form-control" id="Edited_HPNum" name="Edited_HPNum"
-                                   value="<?php echo $HPNum ;?>">
-                        </div>
-                        
-                        <hr class="my-4">
-                        
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-warning">Update!</button>
-                        </div>
-                    </form>
-                </div>
+    </head>
+    <body>
+        <div id="parent-wrapper" class="d-flex flex-column min-vh-100">
+            <?php
+                include "inc.nav.php";
+            ?>
+
+            <div id="breadcrumb-wrapper" class="container mt-3">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="/">Home</a></li>
+                        <li class="breadcrumb-item"><a href="profile.php">Profile</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Edit Profile</li>
+                    </ol>
+                </nav>
             </div>
-            <br>
-            <br>
+
+            <main id="content-wrapper" class="container mb-3">
+                <div class="row justify-content-center">
+                    <div class="col-md-8 col-xl-6">
+                        <div class="card">
+                            <h4 class="card-header bg-info text-white">Edit Profile Information</h4>
+                            <div class="card-body pb-0">
+                                <form action="process/proc.profileedit.php" method="post">
+
+                                    <div class="form-group">
+                                        <label for="Edited_Email">Email Address: </label>
+                                        <input required type="email" class="form-control" id="Edited_Email" name="Edited_Email" value="<?php echo $Email; ?>">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="Edit_Email">First Name: </label>
+                                        <input required maxlength="45" type="text" class="form-control" id="Edited_Fname" name="Edited_Fname" value="<?php echo $Fname; ?>">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="Edit_Email">Last Name: </label>
+                                        <input required maxlength="45" type="text" class="form-control" id="Edited_Lname" name="Edited_Lname" value="<?php echo $Lname; ?>">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="Edit_Email">Phone Number: </label>
+                                        <input required max="99999999" min="60000000" type="number" class="form-control" id="Edited_HPNum" name="Edited_HPNum" value="<?php echo $HPNum; ?>">
+                                    </div>
+                                    <hr class="my-4">
+                                    <div class="form-group text-center">
+                                        <button type="submit" class="btn btn-warning">Update!</button>
+                                    </div>
+                                </form>
+                                <div class="container-fluid pb-3">
+                                    <a href="profile.php" class="btn btn-block btn-primary">Back to Profile</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <?php
+                include "inc.footer.php";
+            ?>
         </div>
-    </div>
+    </body>
 </html>
