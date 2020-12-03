74d195b490a21e67c0b7a1e1b3eda19bae300d05
junhui-f <fjh828@gmail.com>
2020-12-03 22:51:38 +0800

    Minor accessibility tweaks

--- inc.nav.php
+++ inc.nav.php
@@ -16,10 +16,10 @@
         }
     }
 ?>
-<nav class="navbar sticky-top navbar-expand-sm navbar-dark bg-primary">
+<nav class="navbar nav-pills sticky-top navbar-expand-sm navbar-dark bg-primary">
     <a class="navbar-brand" href="/">
         <img id="nav-logo" src="../media/icon/cat.svg" width="30" height="30" alt="Company Logo">
-        Cat Sanctuary
+        <b>Cat Sanctuary</b>
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navBarToggle">
         <span class="navbar-toggler-icon"></span>
@@ -27,13 +27,13 @@
     <div class="collapse navbar-collapse" id="navBarToggle">
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link" href="../index.php">Home</a>
+                <a class="nav-link active" href="../index.php"><b>Home</b></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="../adopt.php">Adopt</a>
+                <a class="nav-link active" href="../adopt.php"><b>Adopt</b></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="../aboutus.php">About Us</a>
+                <a class="nav-link active" href="../aboutus.php"><b>About Us</b></a>
             </li>
             <?php 
             if (isset($_SESSION['SessionId'])){
@@ -49,7 +49,7 @@
         <ul class="navbar-nav ml-auto">
             <li class="nav-item">
                 <a class="nav-link" href="../login.php">
-                    <img style="margin-bottom: 1px;" id="nav-login-logo" src="../media/icon/face-white-48dp.svg" width="30" height="30" alt="Login Icon">
+                    <img style="margin-bottom: 1px;" id="nav-login-logo" src="../media/icon/face-white-48dp.svg" width="35" height="35" alt="Login Icon">
                     <?php echo $Fname; ?>
                 </a>
             </li>
