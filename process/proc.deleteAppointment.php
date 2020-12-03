e4e9ef833c3bd7ffef25bc6d5d53a8e612602211
Chandran996 <roxstar1ha@gmail.com>
2020-12-04 04:59:31 +0800

    Revert "changes to admin navigation"
    This reverts commit 4d287dfd7f39e76744e012185b1354a15ba87e11.

--- process/proc.deleteAppointment.php
+++ process/proc.deleteAppointment.php
@@ -36,7 +36,6 @@ if(mysqli_connect_errno()){
     <body>
          <?php
             include "../inc.nav.php";
-            echo "<br>";
             if ($result && $result2){
                 echo "<div class='container'>";
                 echo "<div class='jumbotron'>";
